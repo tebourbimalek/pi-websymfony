305054bcec4da600b5534be367338ebc5abaf2b1
tebourbimalek <malek.tebourbi@esprit.tn>
2026-05-07 19:22:29 +0100

    Add Symfony assets

---
+++ public/assets/@symfony/stimulus-bundle/controllers-bc84049d4316770e24eabc04185e99a3.js
@@ -0,0 +1,6 @@
+import controller_0 from "../ux-chartjs/controller.js";
+import controller_1 from "../ux-turbo/turbo_controller.js";
+import controller_2 from "../../controllers/hello_controller.js";
+export const eagerControllers = {"symfony--ux-chartjs--chart": controller_0, "symfony--ux-turbo--turbo-core": controller_1, "hello": controller_2};
+export const lazyControllers = {};
+export const isApplicationDebug = false;
